89b76464713775185e3bb09539cb2a6c8348d292
danuri <danualbantani@gmail.com>
2024-06-10 17:59:00 +0300

    ya

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const axios = require("axios");
+const { log } = require('console');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public/')));
+
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+  });
